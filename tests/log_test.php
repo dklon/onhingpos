6e1c98553fe49476c43a9304bb5c9758b0c9e24e
Daniel Kwan <dklon985@gmail.com>
2016-12-21 20:41:25 +0800

    adding tests

---
+++ tests/log_test.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * This file is part of the Money package.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace SebastianBergmann\Money;
+use PHPUnit\Framework\TestCase;
+class MoneyTest extends TestCase
+{
+
+	public function testTest() 
+	{
+
+	}
+}
+
+?>
+
